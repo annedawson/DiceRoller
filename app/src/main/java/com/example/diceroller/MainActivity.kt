5a53674e940f331262408643dc4c108097bb4e5d
annedawson <anne.dawson@gmail.com>
2021-01-10 12:53:28 -0800

    The first commit

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    // here at 10 Jan 2021, 12:48 PT, AD
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
