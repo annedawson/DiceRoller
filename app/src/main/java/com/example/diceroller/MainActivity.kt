aa7f73ea435e57623efcc551713f85c8a73de3aa
annedawson <anne.dawson@gmail.com>
2021-01-12 06:32:38 -0800

    After completing Lesson 1 Concept 16 of https://classroom.udacity.com/courses/ud9012

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     // here at 10 Jan 2021, 12:48 PT, AD
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's Roll"
     }
 }
